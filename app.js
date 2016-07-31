466d6d0109f1c5e271835bbda2e4266d6ba35d23
bovojon <bovojon@gmail.com>
2016-07-31 19:55:59 -0400

    Add states to stateProvider in app.js

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
 (function() {
   'use strict';
 
-  app.module('routerApp', ['ui.router']);
+  app.module('routerApp', ['ui.router'])
+    .config(configFunction);
+
+  configFunction.$inject = ['$stateProvider', '$urlRouterProvider'];
+
+  function configFunction($stateProvider, $urlRouterProvider){
+    $urlRouterProvider.otherwise('/home');
+
+    $stateProvider
+      .state('home', {
+        url: '/home',
+        templateUrl: 'partial-home.html'
+      })
+
+      .state('about', {
+
+      });
+  }
+
 })();
