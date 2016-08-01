d4a20d3776c3b9e8d57456148845683922959fe8
bovojon <bovojon@gmail.com>
2016-08-01 16:08:12 -0400

    Refactor app and wrap JS code in IIFE

--- app.js
+++ app.js
@@ -1,31 +1,32 @@
-// app.js
-var routerApp = angular.module('routerApp', ['ui.router']);
+(function() {
+  'use strict';
 
-routerApp.config(function($stateProvider, $urlRouterProvider) {
+  angular
+    .module('routerApp', ['ui.router'])
+    .config(configFunction)
+
+  configFunction.$inject = ['$stateProvider', '$urlRouterProvider'];
+  function configFunction($stateProvider, $urlRouterProvider) {
 
     $urlRouterProvider.otherwise('/home');
 
     $stateProvider
+      .state('home', {                                                            // HOME STATES AND NESTED VIEWS
+          url: '/home',
+          templateUrl: 'partial-home.html'
+      })
+      .state('home.list', {                                                       // nested list with custom controller
+          url: '/list',
+          templateUrl: 'partial-home-list.html',
+          controller: function($scope) {                                          // Inline custom controller function
+              $scope.mean = ['MongoDB', 'Express', 'Node.js', 'AngularJS'];
+          }
+      })
+      .state('home.paragraph', {                                                  // nested list with just some random string data
+          url: '/paragraph',
+          template: ' XMLHttpRequest is an API that provides client functionality for transferring data between a client and a server.'
+      });
+  }
 
-    // HOME STATES AND NESTED VIEWS ========================================
-    .state('home', {
-        url: '/home',
-        templateUrl: 'partial-home.html'
-    })
 
-    // nested list with custom controller
-    .state('home.list', {
-        url: '/list',
-        templateUrl: 'partial-home-list.html',
-        controller: function($scope) {
-            $scope.dogs = ['Bernese', 'Husky', 'Goldendoodle'];
-        }
-    })
-
-    // nested list with just some random string data
-    .state('home.paragraph', {
-        url: '/paragraph',
-        template: 'I could sure use a drink right now.'
-    });
-
-});
+})();
