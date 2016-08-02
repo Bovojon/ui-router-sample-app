6026a831f1d1d9802c3307d7e43c051e5db789c6
bovojon <bovojon@gmail.com>
2016-08-01 22:30:22 -0400

    Add abouts page

--- app.js
+++ app.js
@@ -4,7 +4,9 @@
   angular
     .module('routerApp', ['ui.router'])
     .config(configFunction)
+    .controller('ScotchController', ScotchController);    // Controllers must be named with captial letters
 
+  /////////////////
   configFunction.$inject = ['$stateProvider', '$urlRouterProvider'];
   function configFunction($stateProvider, $urlRouterProvider) {
 
@@ -26,8 +28,49 @@
           url: '/paragraph',
           template: '<p class="well">'+'In the AngularJS config function, the child states are built using the $stateProvider and defined as:'+'</br>'
                         +'<span class="text-primary">'+'$stateProvider.state( "parentState.childState", {stageConfigObject} )'+'</span>'+'</p>'
-      });
+      })
+      // New view
+      .state('about', {
+        url: '/about',
+        views: {
+
+            // the main template will be placed here (relatively named)
+            '': { templateUrl: 'partial-about.html' },
+
+            // the child views will be defined here (absolutely named)
+            'columnOne@about': { template: 'Look I am a column!' },
+
+            // for column two, we'll define a separate controller
+            'columnTwo@about': {
+                templateUrl: 'table-data.html',
+                controller: 'ScotchController'
+            }
+        }
+
+    });
+  }
+
+  /////////////////
+  ScotchController.$inject = ['$scope'];
+  function ScotchController($scope) {
+    $scope.message = 'test';
+    $scope.scotches = [
+        {
+            name: 'Macallan 12',
+            price: 50
+        },
+        {
+            name: 'Chivas Regal Royal Salute',
+            price: 10000
+        },
+        {
+            name: 'Glenfiddich 1937',
+            price: 20000
+        }
+    ];
+
   }
 
 
+// End of IIFE
 })();
